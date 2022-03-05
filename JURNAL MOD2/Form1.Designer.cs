4028d9c54128c2052413c279d4ce7a75268fb6fa
syabilie <syabilaie@student.telkomuniversity.ac.id>
2022-03-05 17:02:39 +0700

    Add project files.

---
+++ JURNAL MOD2/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace JURNAL_MOD2
+{
+    partial class Kalkulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.b_plus = new System.Windows.Forms.Button();
+            this.b_samadengan = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.text_opr = new System.Windows.Forms.TextBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(15, 96);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(78, 52);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(94, 96);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(78, 52);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(173, 96);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(78, 52);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(15, 147);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(78, 52);
+            this.b4.TabIndex = 3;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(94, 147);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(78, 52);
+            this.b5.TabIndex = 4;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(173, 147);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(78, 52);
+            this.b6.TabIndex = 5;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(15, 198);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(78, 52);
+            this.b7.TabIndex = 6;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(94, 199);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(78, 52);
+            this.b8.TabIndex = 7;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(173, 199);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(78, 52);
+            this.b9.TabIndex = 8;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(94, 251);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(78, 52);
+            this.b0.TabIndex = 9;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // b_plus
+            // 
+            this.b_plus.Location = new System.Drawing.Point(276, 96);
+            this.b_plus.Name = "b_plus";
+            this.b_plus.Size = new System.Drawing.Size(78, 52);
+            this.b_plus.TabIndex = 10;
+            this.b_plus.Text = "+";
+            this.b_plus.UseVisualStyleBackColor = true;
+            this.b_plus.Click += new System.EventHandler(this.b_plus_Click);
+            // 
+            // b_samadengan
+            // 
+            this.b_samadengan.Location = new System.Drawing.Point(276, 147);
+            this.b_samadengan.Name = "b_samadengan";
+            this.b_samadengan.Size = new System.Drawing.Size(78, 52);
+            this.b_samadengan.TabIndex = 11;
+            this.b_samadengan.Text = "=";
+            this.b_samadengan.UseVisualStyleBackColor = true;
+            this.b_samadengan.Click += new System.EventHandler(this.b_samadengan_Click);
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(15, 21);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(339, 47);
+            this.output.TabIndex = 12;
+            this.output.Text = "0";
+            this.output.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // text_opr
+            // 
+            this.text_opr.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.text_opr.Location = new System.Drawing.Point(23, 33);
+            this.text_opr.Name = "text_opr";
+            this.text_opr.Size = new System.Drawing.Size(34, 19);
+            this.text_opr.TabIndex = 13;
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(276, 199);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(78, 51);
+            this.clear.TabIndex = 14;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Kalkulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 328);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.text_opr);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.b_samadengan);
+            this.Controls.Add(this.b_plus);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Kalkulator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button b_plus;
+        private System.Windows.Forms.Button b_samadengan;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox text_opr;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ JURNAL MOD2/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JURNAL_MOD2
+{ 
+    public partial class Kalkulator : Form
+    {
+        decimal bil1;
+        decimal bil2;
+        int oprt;
+        Boolean oprtHasil = false;
+
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "2";
+            }
+            else
+            {
+                output.Text += "2";
+            }
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "7";
+            }
+            else
+            {
+                output.Text += "7";
+            }
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "1";
+            }
+            else
+            {
+                output.Text += "1";
+            }
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "3";
+            }
+            else
+            {
+                output.Text += "3";
+            }
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "4";
+            }
+            else
+            {
+                output.Text += "4";
+            }
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "5";
+            }
+            else
+            {
+                output.Text += "5";
+            }
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "6";
+            }
+            else
+            {
+                output.Text += "6";
+            }
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "8";
+            }
+            else
+            {
+                output.Text += "8";
+            }
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            if (output.Text == "0")
+            {
+                output.Text = "9";
+            }
+            else
+            {
+                output.Text += "9";
+            }
+        }
+
+        private void b0_Click(object sender, EventArgs e)
+        {
+            if(output.Text != "0")
+            {
+                output.Text += "0";
+            }
+        }
+
+        private void b_plus_Click(object sender, EventArgs e)
+        {
+            bil1 = Convert.ToDecimal(output.Text);
+            text_opr.Text = "+";
+            output.Text = " ";
+            oprt = 1;
+            oprtHasil = true;
+        }
+
+        private void b_samadengan_Click(object sender, EventArgs e)
+        {
+            if(oprtHasil == true)
+                bil2 = Convert.ToDecimal(output.Text);
+            {
+                switch (oprt)
+                {
+                    case 1:
+                        output.Text = Convert.ToString(bil1 + bil2);
+                        break;
+                }
+                oprtHasil = false;
+                
+            }
+                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            output.Text = "0";
+            bil1 = 0;
+            bil2 = 0;
+            text_opr.Text = " ";
+        }
+    }
+}
